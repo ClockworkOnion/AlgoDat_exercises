5b16730c7f4ce3d02ea9059feaab3002b41db842
marouane11119999 <marouanesoussi144@gmail.com>
2022-05-11 21:00:01 +0200

    changed

--- ub4/src/main.java
+++ ub4/src/main.java
@@ -1,13 +1,20 @@
 import java.util.Arrays;
+import java.util.Random;
 import java.util.function.Consumer;
 
 public class main {
     public static void main(String[] args) {
-        measureBinarySearchNew(24);
-        //measureLinearSearch(-5);
+        measureLinearSearchConstantValue(-5);
+        measureLinearSearchRandomValues();
+
+        measureBinarySearchForConstant(-2);
+        measureBinarySearchRandomValue();
+
+        measureBinarySearchNewConstantValue(-4);
+        measureBinarySearchNewRandomValues();
     }
-    public static void measureLinearSearch(int Searched) {
-        System.out.println(" Linear Search Measurements : ");
+    public static void measureLinearSearchConstantValue(int Searched) {
+        System.out.println(" Linear Search Measurements With Constant Value: ");
         int[] lengths = { 100000, 200000,1000000,100000000,685154321};
         Arrays.stream(lengths).forEach(x -> {
             int times = 500;
@@ -24,9 +31,52 @@ public class main {
         });
     }
 
-    public static void measureBinarySearch(int Searched) {
+    public static void measureLinearSearchRandomValues() {
+        System.out.println(" Linear Search Measurements With Random Values : ");
+        int[] lengths = { 100000, 200000,1000000,100000000,685154321};
+        Random random=new Random();
+        Arrays.stream(lengths).forEach(x -> {
+            int Searched;
+            int times = 500;
+            long duration = 0;
+            for (int i = 0; i < times; i++) {
+                int[] toSearchIn = SearchTools.createSequenceInc(x);
+                Searched=toSearchIn[random.nextInt(0,toSearchIn.length-1)];
+                System.gc();
+                long before = System.nanoTime();
+                SearchTools.linSearch(toSearchIn,Searched);
+                duration += System.nanoTime() - before;
+            }
+            duration = duration / times;
+            System.out.println("Average time of searching  in " + times + " measurements for length " + x + ": " + duration);
+        });
+    }
+
+    public static void measureBinarySearchRandomValue() {
+        System.out.println(" Binary Search Measurements for Random values : ");
+        int[] lengths = { 100000, 200000,1000000,100000000,685154321};
+        Random random=new Random();
+        Arrays.stream(lengths).forEach(x -> {
+            int Searched;
+            int times = 500;
+            long duration = 0;
+            for (int i = 0; i < times; i++) {
+                int[] toSearchIn = SearchTools.createSequenceInc(x);
+                Searched=toSearchIn[random.nextInt(0,toSearchIn.length-1)];
+                System.gc();
+                long before = System.nanoTime();
+                SearchTools.binSearch(toSearchIn,Searched,0,toSearchIn.length-1);
+                duration += System.nanoTime() - before;
+            }
+            duration = duration / times;
+            System.out.println("Average time of searching in " + times + " measurements for length " + x + ": " + duration);
+        });
+    }
+
+    public static void measureBinarySearchForConstant(int Searched) {
         System.out.println(" Binary Search Measurements : ");
         int[] lengths = { 100000, 200000,1000000,100000000,685154321};
+        Random random=new Random();
         Arrays.stream(lengths).forEach(x -> {
             int times = 500;
             long duration = 0;
@@ -38,12 +88,12 @@ public class main {
                 duration += System.nanoTime() - before;
             }
             duration = duration / times;
-            System.out.println("Average time of searching "+Searched+" in " + times + " measurements for length " + x + ": " + duration);
+            System.out.println("Average time of searching in " + times + " measurements for length " + x + ": " + duration);
         });
     }
 
-    public static void measureBinarySearchNew(int Searched) {
-        System.out.println(" Binary Search Measurements : ");
+    public static void measureBinarySearchNewConstantValue(int Searched) {
+        System.out.println(" Binary Search Measurements With Constant Value : ");
         int[] lengths = { 100000, 200000,1000000,100000000,685154321};
         Arrays.stream(lengths).forEach(x -> {
             int times = 500;
@@ -57,6 +107,27 @@ public class main {
             }
             duration = duration / times;
             System.out.println("Average time of searching "+Searched+" in " + times + " measurements for length " + x + ": " + duration);
+        });
+    }
+
+    public static void measureBinarySearchNewRandomValues() {
+        System.out.println(" Binary Search Measurements With Random Values: ");
+        int[] lengths = { 100000, 200000,1000000,100000000,685154321};
+        Random random=new Random();
+        Arrays.stream(lengths).forEach(x -> {
+            int Searched;
+            int times = 500;
+            long duration = 0;
+            for (int i = 0; i < times; i++) {
+                int[] toSearchIn = SearchTools.createSequenceInc(x);
+                Searched=toSearchIn[random.nextInt(0,toSearchIn.length-1)];
+                System.gc();
+                long before = System.nanoTime();
+                SearchTools.binSearchNew(toSearchIn,Searched,0,toSearchIn.length-1);
+                duration += System.nanoTime() - before;
+            }
+            duration = duration / times;
+            System.out.println("Average time of searching  in " + times + " measurements for length " + x + ": " + duration);
         });
     }
 
