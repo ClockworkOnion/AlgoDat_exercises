850efb9e05826c818b22ee9cdb60fd8a6de941cb
ClockworkOnion <happella@students.uni-marburg.de>
2022-05-13 16:06:40 +0200

    added runtimes as comment

--- ub4/src/main.java
+++ ub4/src/main.java
@@ -2,10 +2,48 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Consumer;
+ /*
+         Binary Search Measurements :
+         Average time of searching in 500 measurements for length 100000: 2800
+         Average time of searching in 500 measurements for length 200000: 1231
+         Average time of searching in 500 measurements for length 1000000: 1189
+         Average time of searching in 500 measurements for length 100000000: 1607
+         Average time of searching in 500 measurements for length 685154321: 3721
+         Binary Search Measurements for Random values :
+         Average time of searching in 500 measurements for length 100000: 3588
+         Average time of searching in 500 measurements for length 200000: 1885
+         Average time of searching in 500 measurements for length 1000000: 3018
+         Average time of searching in 500 measurements for length 100000000: 5458
+         Average time of searching in 500 measurements for length 685154320: 6992
+         Binary Search New Measurements With Constant Value :
+         Average time of searching -4 in 500 measurements for length 100000: 2122
+         Average time of searching -4 in 500 measurements for length 200000: 1545
+         Average time of searching -4 in 500 measurements for length 1000000: 1512
+         Average time of searching -4 in 500 measurements for length 100000000: 2087
+         Average time of searching -4 in 500 measurements for length 685154321: 4944
+         Binary Search Measurements With Random Values:
+         Average time of searching  in 500 measurements for length 100000: 1907
+         Average time of searching  in 500 measurements for length 200000: 2116
+         Average time of searching  in 500 measurements for length 1000000: 2014
+         Average time of searching  in 500 measurements for length 100000000: 3978
+         Average time of searching  in 500 measurements for length 685154321: 7031
+         Linear Search Measurements With Constant Value:
+         Average time of searching -5 in 500 measurements for length 100000: 30615
+         Average time of searching -5 in 500 measurements for length 200000: 50843
+         Average time of searching -5 in 500 measurements for length 1000000: 286819
+         Average time of searching -5 in 500 measurements for length 100000000: 27886644
+         Average time of searching -5 in 500 measurements for length 685154321: 190361036
+         Linear Search Measurements With Random Values :
+         Average time of searching  in 500 measurements for length 100000: 16761
+         Average time of searching  in 500 measurements for length 200000: 28164
+         Average time of searching  in 500 measurements for length 1000000: 140109
+         Average time of searching  in 500 measurements for length 100000000: 13747576
+
+  */
 
 public class main {
     public static void main(String[] args) {
-       measureBinarySearchForConstant(-2);
+    //   measureBinarySearchForConstant(-2);
        measureBinarySearchRandomValue();
 
        measureBinarySearchNewConstantValue(-4);
@@ -20,8 +58,8 @@ public class main {
         Arrays.stream(lengths).forEach(x -> {
             int times = 500;
             long duration = 0;
-            for (int i = 0; i < times; i++) {
                 int[] toSearchIn = SearchTools.createSequenceInc(x);
+            for (int i = 0; i < times; i++) {
                 System.gc();
                 long before = System.nanoTime();
                 SearchTools.linSearch(toSearchIn,Searched);
@@ -40,8 +78,8 @@ public class main {
             int Searched;
             int times = 500;
             long duration = 0;
-            for (int i = 0; i < times; i++) {
                 int[] toSearchIn = SearchTools.createSequenceInc(x);
+            for (int i = 0; i < times; i++) {
                 Searched=toSearchIn[ThreadLocalRandom.current().nextInt(0, toSearchIn.length-1)];
                 System.gc();
                 long before = System.nanoTime();
@@ -55,15 +93,15 @@ public class main {
 
     public static void measureBinarySearchRandomValue() {
         System.out.println(" Binary Search Measurements for Random values : ");
-//        int[] lengths = { 100000, 200000,1000000,100000000,685154320};
-        int[] lengths = { 100000000,685154320};
+        int[] lengths = { 100000, 200000,1000000,100000000,685154320};
+      //  int[] lengths = { 100000000,685154320};
         Random random=new Random();
         Arrays.stream(lengths).forEach(x -> {
             int Searched;
             int times = 500;
             long duration = 0;
-            for (int i = 0; i < times; i++) {
                 int[] toSearchIn = SearchTools.createSequenceInc(x);
+            for (int i = 0; i < times; i++) {
                 Searched=toSearchIn[ThreadLocalRandom.current().nextInt(0, toSearchIn.length-1)];
                 System.gc();
                 long before = System.nanoTime();
@@ -82,8 +120,8 @@ public class main {
         Arrays.stream(lengths).forEach(x -> {
             int times = 500;
             long duration = 0;
-            for (int i = 0; i < times; i++) {
                 int[] toSearchIn = SearchTools.createSequenceInc(x);
+            for (int i = 0; i < times; i++) {
                 System.gc();
                 long before = System.nanoTime();
                 SearchTools.binSearch(toSearchIn,Searched,0,toSearchIn.length-1);
@@ -100,8 +138,8 @@ public class main {
         Arrays.stream(lengths).forEach(x -> {
             int times = 500;
             long duration = 0;
-            for (int i = 0; i < times; i++) {
                 int[] toSearchIn = SearchTools.createSequenceInc(x);
+            for (int i = 0; i < times; i++) {
                 System.gc();
                 long before = System.nanoTime();
                 SearchTools.binSearchNew(toSearchIn,Searched,0,toSearchIn.length-1);
@@ -120,8 +158,8 @@ public class main {
             int Searched;
             int times = 500;
             long duration = 0;
-            for (int i = 0; i < times; i++) {
                 int[] toSearchIn = SearchTools.createSequenceInc(x);
+            for (int i = 0; i < times; i++) {
                 Searched=toSearchIn[ThreadLocalRandom.current().nextInt(0, toSearchIn.length-1)];
                 System.gc();
                 long before = System.nanoTime();
