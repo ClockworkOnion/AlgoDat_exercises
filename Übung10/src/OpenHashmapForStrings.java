3db34b98cd287d89282d649794a1ca6b7c60c170
marouane11119999 <marouanesoussi144@gmail.com>
2022-06-24 01:00:34 +0200

    last

--- Übung10/src/OpenHashmapForStrings.java
+++ Übung10/src/OpenHashmapForStrings.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.constant.Constable;
+import java.math.BigInteger;
+import java.util.Objects;
+import java.util.function.BiFunction;
 
 public class OpenHashmapForStrings {
     public  String[] OpenHashMapArray;
@@ -22,8 +24,16 @@ public class OpenHashmapForStrings {
         return sum;
     }
 
+    public static int  radixRepresentation(String string){
+        int sum=0;
+        for (int i = string.length()-1; i >=0 ; i--) {
+            sum+=((int) string.charAt(i))*Math.pow(128,string.length()-1-i);
+        }
+        return sum;
+    }
+
     public int hashing(String str){
-        return asciiOfString(str)%OpenHashMapArray.length;
+        return radixRepresentation(str)%OpenHashMapArray.length;
     }
 
     public  int linearHashing(String str,int i){
@@ -31,20 +41,41 @@ public class OpenHashmapForStrings {
     }
 
     public  int quadraticHashing(String str,int i){
-        return (hashing(str)+i)% OpenHashMapArray.length;
+        return (int) ((hashing(str)+i*c1+Math.pow(i,2)*c2)% OpenHashMapArray.length);
     }
 
     public int doubledHashing(String str,int i){
-        return (hashing(str) + i*(1+asciiOfString(str)%(OpenHashMapArray.length)-1));
+        int m_1=OpenHashMapArray.length-1;
+        long x=hashing(str);
+        long y= (long) i *(1+(radixRepresentation(str)%m_1));
+        long result = (x+y)%OpenHashMapArray.length;
+        return  (int) result;
     }
 
 
-    public  int insert(String str) throws CustomException{
-        int i=0,j;
-        while (i< OpenHashMapArray.length){
-            j=linearHashing(str,i);
-            if (OpenHashMapArray[j] == "Deleted" || OpenHashMapArray[j]==null){
-                OpenHashMapArray[j]=str;
+
+//    public  int insert(String str) throws CustomException{
+//        int i=0,j=0;
+//        while (i< OpenHashMapArray.length ){
+//            j=doubledHashing(str,i);
+//            if (Objects.equals(OpenHashMapArray[j], "Deleted") || OpenHashMapArray[j]==null){
+//                OpenHashMapArray[j]=str;
+//                return j;
+//            }
+//            else i++;
+//            //if (i==OpenHashMapArray.length && !OpenHashMapArray[j].equals(str)) i--;
+//        }
+//        throw  new CustomException("Überlauf");
+//    }
+
+    public  int insert(String str, BiFunction<String, Integer, Integer> hash) throws CustomException{
+        int i=0;
+        int j=0;
+        while (i< OpenHashMapArray.length ){
+            j= hash.apply(str, i);
+           // System.out.println(str+"  "+i);
+            if (Objects.equals(OpenHashMapArray[(int) j], "Deleted") || OpenHashMapArray[(int) j]==null){
+                OpenHashMapArray[(int) j]=str;
                 return j;
             }
             else i++;
@@ -52,23 +83,33 @@ public class OpenHashmapForStrings {
         throw  new CustomException("Überlauf");
     }
 
-    public void addDataFromFile(String fileName) throws IOException, CustomException {
+//    public void addDataFromFile(String fileName) throws IOException, CustomException {
+//        File file = new File(fileName);
+//        BufferedReader br = new BufferedReader(new FileReader(file));
+//        String st;
+//        while ((st = br.readLine()) != null){
+//            insert(st);
+//        }
+//    }
+
+    public void addDataFromFile(String fileName,BiFunction<String, Integer, Integer> hash) throws IOException, CustomException {
         File file = new File(fileName);
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
         while ((st = br.readLine()) != null){
-            insert(st);
+            insert(st,hash);
         }
     }
 
-    public int search(String str){
-        int i=0,j;
+    public int search(String str, BiFunction<String, Integer, Integer> hash){
+        int i=0;
+        int j;
         while(i< OpenHashMapArray.length){
-            j=linearHashing(str,i);
-            if (OpenHashMapArray[j]==null){
+            j= hash.apply(str, i);
+            if (OpenHashMapArray[(int) j]==null){
                 return -1;
             }
-            if (OpenHashMapArray[j].equals(str)){
+            if (OpenHashMapArray[(int) j].equals(str)){
                 return j;
             }
             i++;
@@ -76,19 +117,21 @@ public class OpenHashmapForStrings {
         return -1;
     }
 
-    public void delete(String str){
-        int j= search(str);
+    public void delete(String str,BiFunction<String, Integer, Integer> hash){
+
+        int j= search(str,hash);
+        //System.out.println(j);
         if (j != -1){
-            OpenHashMapArray[j]="Deleted";
+            OpenHashMapArray[(int) j]="Deleted";
         }
     }
 
-    public int countSuccededSearchs(String fileName) throws IOException{
+    public int countNotSuccededSearchs(String fileName,BiFunction<String, Integer, Integer> hash) throws IOException{
         File file = new File(fileName);
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st; int c=0;
         while ((st = br.readLine()) != null){
-            if (search(st)!=-1){
+            if (search(st,hash)!=-1){
                 c++;
             }
         }
@@ -101,12 +144,44 @@ public class OpenHashmapForStrings {
         }
     }
 
+    public  void measureInserting(String Filename,BiFunction<String, Integer, Integer> hash) {
+        System.out.println(" Inserting Measurements : ");
+        long duration = 0;
+        long before = System.nanoTime();
+        try {
+                this.addDataFromFile(Filename,hash);
+            } catch (IOException | CustomException e) {
+                e.printStackTrace();
+            }
+        duration += System.nanoTime() - before;
+        System.out.println("Average time of inserting in array with length "+this.OpenHashMapArray.length+" is "  + duration);
+    }
+
+    public  void measureSearching(String Filename,BiFunction<String, Integer, Integer> hash) {
+        System.out.println(" Inserting Measurements : ");
+        int p = 0;
+        long duration = 0;
+        long before = System.nanoTime();
+        try {
+            p=countNotSuccededSearchs(Filename,hash);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        duration += System.nanoTime() - before;
+        System.out.println("Average time of searching is "  + duration +" with : "+p+" Unsuccessful searches.");
+    }
+
     public static void main(String[] args) throws IOException, CustomException {
-        OpenHashmapForStrings object=new OpenHashmapForStrings(433);
-        object.addDataFromFile("C:\\Users\\21260\\Desktop\\Übung10\\bb.txt");
-        object.delete("a");
-        //System.out.println(object.search("a"));
-        object.printHashArray();
+        //OpenHashmapForStrings object1=new OpenHashmapForStrings(200000);
+        OpenHashmapForStrings object2=new OpenHashmapForStrings(200000);
+//        object1.measureInserting("C:\\Users\\21260\\Desktop\\Übung10\\OfficialScrabbleWordListGerman.txt",object1::linearHashing);
+//       object2.measureInserting("C:\\Users\\21260\\Desktop\\Übung10\\OfficialScrabbleWordListGerman.txt",object2::linearHashing);
+        //System.out.println( radixRepresentation("pto"));
+        object2.addDataFromFile("C:\\Users\\21260\\Desktop\\Übung10\\OfficialScrabbleWordListGerman.txt",object2::doubledHashing);
+        //int x=4420*(radixRepresentation("ABTRUMPFE")%(object2.OpenHashMapArray.length-1));
+        //int y=object2.hashing("ABTRUMPFE");//4420
+        //System.out.println(object2.doubledHashing("ABTRUMPFE",4420));
+
     }
 
 }
